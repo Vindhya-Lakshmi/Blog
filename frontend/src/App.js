400c5f2e54b24a3b4efb9508a3ce091b11f999a1
Vindhya <vindhyalakshmiofficial@gmail.com>
2026-04-17 10:36:06 +0530

    style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
 import Login from "./pages/Login";
 import Blogs from "./pages/Blogs";
-
 function App() {
   return (
     <div>
+<h1 className="text-5xl text-red-500">Test Tailwind</h1>  
       <h1>My Blog App </h1>
       <Login />
       <Blogs />
